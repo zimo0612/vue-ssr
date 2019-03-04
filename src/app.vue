4a19a36da6c99b1fff08da462295f4f8c31e27db
liuyanan <liuyanan@huitong.com>
2019-03-04 20:12:40 +0800

    3-3

---
+++ src/app.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div id="cover"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      text: 'abcde'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+#app{
+  position absolute
+  left 0
+  right 0
+  top 0
+  bottom 0
+}
+#cover{
+  position absolute
+  left 0
+  top 0
+  right 0
+  bottom 0
+  background-color #999999
+  opacity .9
+  z-index -1
+}
+</style>
